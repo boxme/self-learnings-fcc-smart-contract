79e86febf2d8287d91642862e28159ab4079f094
boxme <desmond.ng.yang.yi@gmail.com>
2022-10-16 21:06:27 +0800

    Upload image metadata to pinata

--- hardhat-nft-fcc/deploy/02-deploy-random-ipfs-nft.js
+++ hardhat-nft-fcc/deploy/02-deploy-random-ipfs-nft.js
@@ -1,8 +1,24 @@
 const { network } = require("hardhat");
 const { networkConfig, developmentChains } = require("../helper-hardhat-config");
-const { storeImages } = require("../utils/uploadToPinata");
+const { storeImages, storeTokenUriMetadata } = require("../utils/uploadToPinata");
 
 const imagesLocation = "./images/randomNft/";
+const metadataTemplate = {
+    name: "",
+    description: "",
+    image: "",
+    attributes: [
+        {
+            trait_type: "Cuteness",
+            value: 100,
+        },
+    ],
+};
+let tokenUris = [
+    "ipfs://QmaVkBn2tKmjbhphU7eyztbvSQU5EXDdqRyXZtRhSGgJGo",
+    "ipfs://QmYQC5aGZu2PTH8XzbJrbDnvhj3gVs7ya33H9mqUNvST3d",
+    "ipfs://QmZYmH5iDbD6v3U2ixoVAjioSzvWJszDzYdbeCLquGSpVm",
+];
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments;
@@ -27,7 +43,9 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
         subscriptionId = networkConfig[chainId].subscriptionId;
     }
     log("----------------------------------------------------");
-    await handleTokenUris();
+    if (process.env.UPLOAD_TO_PINATA == "true") {
+        tokenUris = await handleTokenUris();
+    }
     // const args = [
     //     vrfCoordinatorV2Address,
     //     networkConfig[chainId].gasLane,
@@ -38,9 +56,26 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
 };
 
 async function handleTokenUris() {
+    // Check out https://github.com/PatrickAlphaC/nft-mix for a pythonic version of uploading
+    // to the raw IPFS-daemon from https://docs.ipfs.io/how-to/command-line-quick-start/
+    // You could also look at pinata https://www.pinata.cloud/
+
     tokenUris = [];
-    const { responses, files } = await storeImages(imagesLocation);
-    console.log(responses);
+    const { responses: imageUploadResponses, files } = await storeImages(imagesLocation);
+    for (imageUploadResponseIndex in imageUploadResponses) {
+        let tokenUriMetadata = { ...metadataTemplate };
+        // Replace pug.png to pug
+        tokenUriMetadata.name = files[imageUploadResponseIndex].replace(".png", "");
+        tokenUriMetadata.description = `An adorable ${tokenUriMetadata.name} pup!`;
+        tokenUriMetadata.image = `ipfs://${imageUploadResponses[imageUploadResponseIndex].IpfsHash}`;
+        console.log(`Uploading ${tokenUriMetadata.name}...`);
+
+        // Store JSON to pinata / IPFS
+        const metadataUploadResponse = await storeTokenUriMetadata(tokenUriMetadata);
+        tokenUris.push(`ipfs://${metadataUploadResponse.IpfsHash}`);
+    }
+    console.log("Token URIs uploaded! They are:");
+    console.log(tokenUris);
 
     return tokenUris;
 }
