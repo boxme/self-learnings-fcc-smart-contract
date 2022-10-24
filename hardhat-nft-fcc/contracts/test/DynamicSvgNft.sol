201ae70d9fe102458a4d84a4e3497456f394dd67
boxme <desmond.ng.yang.yi@gmail.com>
2022-10-24 11:42:01 +0800

    Dynamic SVG Nft sol contract code

---
+++ hardhat-nft-fcc/contracts/test/DynamicSvgNft.sol
@@ -0,0 +1,100 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "base64-sol/base64.sol";
+
+error ERC721Metadata__URI_QueryFor_NonExistentToken();
+
+contract DynamicSvgNft is ERC721 {
+    uint256 private s_tokenCounter;
+    string private s_lowImageUri;
+    string private s_highImageUri;
+
+    mapping(uint256 => int256) private s_tokenIdToHighValues;
+    AggregatorV3Interface internal immutable i_priceFeed;
+
+    event CreatedNft(uint256 indexed tokenId, int256 highValue);
+
+    constructor(
+        address priceFeedAddress,
+        string memory lowSvg,
+        string memory highSvg
+    ) ERC721("Dynamic SVG NFT", "DSN") {
+        s_tokenCounter = 0;
+        i_priceFeed = AggregatorV3Interface(priceFeedAddress);
+        s_lowImageUri = svgToImageURI(lowSvg);
+        s_highImageUri = svgToImageURI(highSvg);
+    }
+
+    function svgToImageURI(string memory svg) public pure returns (string memory) {
+        // example:
+        // '<svg width="500" height="500" viewBox="0 0 285 350" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="black" d="M150,0,L75,200,L225,200,Z"></path></svg>'
+        // would return ""
+        string memory baseURL = "data:image/svg+xml;base64,";
+        string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
+        return string(abi.encodePacked(baseURL, svgBase64Encoded));
+    }
+
+    function mintNft(int256 highValue) public {
+        uint256 newTokenId = s_tokenCounter;
+        s_tokenIdToHighValues[newTokenId] = highValue;
+        s_tokenCounter = s_tokenCounter + 1;
+        _safeMint(msg.sender, newTokenId);
+
+        emit CreatedNft(newTokenId, highValue);
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        if (!_exists(tokenId)) {
+            revert ERC721Metadata__URI_QueryFor_NonExistentToken();
+        }
+
+        (, int256 price, , , ) = i_priceFeed.latestRoundData();
+        string memory imageURI = s_lowImageUri;
+        if (price >= s_tokenIdToHighValues[tokenId]) {
+            imageURI = s_highImageUri;
+        }
+
+        // Encodes JSON payload
+        return
+            string(
+                abi.encodePacked(
+                    _baseURI(),
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{"name":"',
+                                name(), // You can add whatever name here
+                                '", "description":"An NFT that changes based on the Chainlink Feed", ',
+                                '"attributes": [{"trait_type": "coolness", "value": 100}], "image":"',
+                                imageURI,
+                                '"}'
+                            )
+                        )
+                    )
+                )
+            );
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
+    }
+
+    function getLowSVG() public view returns (string memory) {
+        return s_lowImageUri;
+    }
+
+    function getHighSVG() public view returns (string memory) {
+        return s_highImageUri;
+    }
+
+    function getPriceFeed() public view returns (AggregatorV3Interface) {
+        return i_priceFeed;
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
+    }
+}
