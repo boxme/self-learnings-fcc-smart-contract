62cf6bc71504742b63498a698e771ca3e3f047aa
boxme <desmond.ng.yang.yi@gmail.com>
2022-11-15 18:21:52 +0800

    Marketplace listing homepage

--- nextjs-nft-marketplace-fcc-moralis/pages/_app.js
+++ nextjs-nft-marketplace-fcc-moralis/pages/_app.js
@@ -2,9 +2,16 @@ import "../styles/globals.css";
 import { MoralisProvider } from "react-moralis";
 import Header from "../components/Header";
 import Head from "next/head";
+import { NotificationProvider } from "web3uikit";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const APP_ID = process.env.NEXT_PUBLIC_APP_ID;
 const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
+
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: "https://api.studio.thegraph.com/query/38062/nft-marketplace/v0.0.1",
+});
 
 function MyApp({ Component, pageProps }) {
     return (
@@ -15,8 +22,12 @@ function MyApp({ Component, pageProps }) {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <MoralisProvider initializeOnMount={false}>
-                <Header />
-                <Component {...pageProps} />
+                <ApolloProvider client={client}>
+                    <NotificationProvider>
+                        <Header />
+                        <Component {...pageProps} />
+                    </NotificationProvider>
+                </ApolloProvider>
             </MoralisProvider>
         </div>
     );
--- nextjs-nft-marketplace-fcc-moralis/pages/index.js
+++ nextjs-nft-marketplace-fcc-moralis/pages/index.js
@@ -1,6 +1,48 @@
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import networkMapping from "../constants/networkMapping.json";
+import { useMoralis } from "react-moralis";
+import GET_ACTIVE_ITEM from "../constants/subgraphQueries";
+import { useQuery } from "@apollo/client";
+import NFTBox from "../components/NFTBox";
+import { useEffect } from "react";
 
 export default function Home() {
-    return <div className={styles.container}>Hi!</div>;
+    const { web3, enableWeb3, isWeb3Enabled, isWeb3EnableLoading, chainId } = useMoralis();
+    const chainString = chainId ? parseInt(chainId).toString() : "31337";
+    const marketplaceAddress = networkMapping[chainString].NftMarketplace[0];
+
+    const { loading, error, data: listedNfts } = useQuery(GET_ACTIVE_ITEM);
+
+    return (
+        <div className="container mx-auto">
+            <h1 className="py-4 px-4 font-bold text-2xl">Recently Listed</h1>
+            <div className="flex flex-wrap">
+                {isWeb3Enabled ? (
+                    loading || !listedNfts ? (
+                        <div>Loading...</div>
+                    ) : (
+                        listedNfts.activeItems.map((nft) => {
+                            console.log(nft);
+                            const { price, nftAddress, tokenId, seller } = nft;
+                            return (
+                                <NFTBox
+                                    price={price}
+                                    nftAddress={nftAddress}
+                                    tokenId={tokenId}
+                                    marketplaceAddress={marketplaceAddress}
+                                    seller={seller}
+                                    key={`${nftAddress}${tokenId}`}
+                                />
+                            );
+                        })
+                    )
+                ) : (
+                    <button onClick={() => enableWeb3()} disabled={isWeb3EnableLoading}>
+                        Enable web3
+                    </button>
+                )}
+            </div>
+        </div>
+    );
 }
